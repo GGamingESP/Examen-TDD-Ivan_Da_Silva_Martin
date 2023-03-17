913007994ebc42d5a0f7798032558646fa96c272
Ivan Da Silva Martin <93001680+GGamingESP@users.noreply.github.com>
2023-03-17 18:30:21 +0000

    Delete Ejercicio1.js

--- Ejercicio1.js
+++
@@ -1,8 +0,0 @@
-export const fizzbuzz = (number) => {
-  if (typeof number !== 'number') throw new Error('The provided parameter must be a number')
-  if (Number.isNaN(number)) throw new Error('The provided parameter must be a number')
-  if (number % 2 === 0) return 'Ivan'
-  if (number % 3 === 0) return 'Da Silva'
-  if (number % 11 === 0) return 'Martin'
-  return number
-}
