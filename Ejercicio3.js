dec2dd06c86eafd741356244f6b6b95fcf380803
Ivan Da Silva Martin <93001680+GGamingESP@users.noreply.github.com>
2023-03-17 18:30:48 +0000

    Delete Ejercicio3.js

--- Ejercicio3.js
+++
@@ -1,7 +0,0 @@
-export const distributegiftIDSM = (packsofGifts, reindeers) => {
-  if (typeof packsofGifts !== 'string') throw new Error('debe ser un string')
-  if (typeof reindeers !== 'string') throw new Error('debe ser un string')
-  const pesoreno = reindeers.length / 4
-  const pesoregalos = packsofGifts.length
-  return Math.floor(pesoreno / pesoregalos)
-}
