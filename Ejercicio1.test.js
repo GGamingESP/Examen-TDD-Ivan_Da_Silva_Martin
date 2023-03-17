fb2d7497c15d063d81819ce6efb21ba3375d8e1a
Ivan Da Silva Martin <93001680+GGamingESP@users.noreply.github.com>
2023-03-17 18:23:46 +0000

    Add files via upload

---
+++ Ejercicio1.test.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import { describe, it } from 'vitest'
+import { fizzbuzz } from './Ejercicio1'
+// const fizzbuzz = (number) => {
+//   if (typeof number !== 'number') throw new Error('The provided parameter must be a number')
+//   if (Number.isNaN(number)) throw new Error('The provided parameter must be a number')
+//   if (number % 2 === 0) return 'Ivan'
+//   if (number % 3 === 0) return 'Da Silva'
+//   if (number % 11 === 0) return 'Martin'
+//   return number
+// }
+
+describe('fizzbuzz', () => {
+  it('debería ser una funcion', () => {
+    expect(typeof fizzbuzz).toBe('function')
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(() => fizzbuzz()).toThrow('The provided parameter must be a number')
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(() => fizzbuzz()).toThrow('The provided parameter must be a number')
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(() => fizzbuzz()).toThrow('The provided parameter must be a number')
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(fizzbuzz(1)).toBe(1)
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(fizzbuzz(2)).toBe('Ivan')
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(fizzbuzz(3)).toBe('Da Silva')
+  })
+  it('debería fallar si no es un númnero el parámetro proporcionado', () => {
+    expect(fizzbuzz(11)).toBe('Martin')
+  })
+})
